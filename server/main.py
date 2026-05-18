3a6983aec821e6183df9f6dd84a53009d12ee5c0
Sumitpal321 <sumitkumar71496@gmail.com>
2026-05-18 08:42:00 +0530

    Fix CORS for Railway

--- server/main.py
+++ server/main.py
@@ -8,11 +8,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-    "http://localhost:3000",
-    "https://your-vercel-app.vercel.app"
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
